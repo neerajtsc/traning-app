65541bea15b251d978b611ab56343fabd5b780e0
Neeraj verma <neeraj.verma@techsuperiors.com>
2024-04-26 12:32:23 +0530

    code fix

--- src/app/online-compiler/layout.tsx
+++
@@ -1,9 +0,0 @@
-import "../globals.css";
-export const metadata = {
-  title: 'Online Compiler',
-  description: 'Generated ',
-};
-
-export default function OnlineLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
-}
--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -134,13 +134,13 @@ export default function Navbar() {
                                         </NavigationMenuLink>
                                     </Link>
                                 </NavigationMenuItem>
-                                <NavigationMenuItem>
+                                {/* <NavigationMenuItem>
                                     <Link href="/online-compiler" legacyBehavior passHref>
                                         <NavigationMenuLink className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground">
                                             Online Compiler
                                         </NavigationMenuLink>
                                     </Link>
-                                </NavigationMenuItem>
+                                </NavigationMenuItem> */}
                             </NavigationMenuList>
                         </NavigationMenu>
                     </nav>
