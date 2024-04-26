9c62e974bf65d84b93ffaf7560c932dfc09fb73c
Neeraj verma <neeraj.verma@techsuperiors.com>
2024-04-26 12:27:54 +0530

    build issue fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -370,7 +370,7 @@ export default function Home() {
                       <div className="flex items-center mb-4">
                         <img className="w-10 h-10 me-4 rounded-full" src={`https://randomuser.me/api/portraits/men/${20 + index}.jpg`} alt="d" />
                         <div className="font-medium dark:text-white">
-                          <p>Jese Leos <time datetime="2014-08-16 19:00" className="block text-sm text-gray-500 dark:text-gray-400">Joined on August 2014</time></p>
+                          <p>Jese Leos <time  className="block text-sm text-gray-500 dark:text-gray-400">Joined on August 2014</time></p>
                         </div>
                       </div>
                       <div className="flex items-center mb-1 space-x-1 rtl:space-x-reverse">
@@ -391,7 +391,7 @@ export default function Home() {
                         </svg>
                         <h3 className="ms-2 text-sm font-semibold text-gray-900 dark:text-white">Thinking to buy another one!</h3>
                       </div>
-                      <footer className="mb-5 text-sm text-gray-500 dark:text-gray-400"><p>Reviewed in the United Kingdom on <time datetime="2017-03-03 19:00">March 3, 2017</time></p></footer>
+                      <footer className="mb-5 text-sm text-gray-500 dark:text-gray-400"><p>Reviewed in the United Kingdom on <time>March 3, 2017</time></p></footer>
                       <p className="mb-2 text-gray-500 dark:text-gray-400">This is my third Invicta Pro Diver. They are just fantastic value for money. This one arrived yesterday and the first thing I did was set the time, popped on an identical strap from another Invicta and went in the shower with it to test the waterproofing.... No problems.</p>
                       <p className="mb-3 text-gray-500 dark:text-gray-400">It is obviously not the same build quality as those very expensive watches. But that is like comparing a Citroën to a Ferrari. This watch was well under £100! An absolute bargain.</p>
                       <a href="#" className="block mb-5 text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">Read more</a>
@@ -439,7 +439,7 @@ export default function Home() {
 
                 <div className="col-span-2 lg:col-span-3 lg:flex lg:items-end">
                   <form className="w-full">
-                    <label for="UserEmail" className="sr-only"> Email </label>
+                    <label htmlFor="UserEmail" className="sr-only"> Email </label>
 
                     <div
                       className="border border-gray-100 p-2 focus-within:ring sm:flex sm:items-center sm:gap-4"
