9f5ebeb1820c9f9f95314676957bcf08cc5c1b7b
AkshaypPandey <akshay.pandey@techsuperiors.com>
2024-05-13 15:15:52 +0530

    blog section added

---
+++ src/components/ui/blog.tsx
@@ -0,0 +1,78 @@
+import "../../../styles/blog.css";
+import React, { useState } from "react";
+import Image from "next/image";
+const blog = () => {
+    const cardData = [
+        {
+            title:"HTML, CSS",
+            src:"/images/htmlCss.jpg",
+            desc:"Embark on your journey into the world of web development by mastering the fundamental building blocks: HTML and CSS. In this beginner-friendly guide, you'll learn the essentials of structuring web pages with HTML tags and styling them with CSS properties."
+        },
+        {
+            title:"JS",
+            src:"/images/jScript.jpg",
+            desc:"Embark on your journey into the world of web development by mastering the fundamental building blocks: HTML and CSS. In this beginner-friendly guide, you'll learn the essentials of structuring web pages with HTML tags and styling them with CSS properties."
+        },
+        {
+            title:"React",
+            src:"/images/react.jpg",
+            desc:"Embark on your journey into the world of web development by mastering the fundamental building blocks: HTML and CSS. In this beginner-friendly guide, you'll learn the essentials of structuring web pages with HTML tags and styling them with CSS properties."
+        },
+        {
+            title:"HTML, CSS",
+            src:"/images/jScript.jpg",
+            desc:"Embark on your journey into the world of web development by mastering the fundamental building blocks: HTML and CSS. In this beginner-friendly guide, you'll learn the essentials of structuring web pages with HTML tags and styling them with CSS properties."
+        },
+        {
+            title:"HTML/CSS",
+            src:"/images/htmlCss.jpg",
+            desc:"Embark on your journey into the world of web development by mastering the fundamental building blocks: HTML and CSS. In this beginner-friendly guide, you'll learn the essentials of structuring web pages with HTML tags and styling them with CSS properties."
+        },
+        {
+            title:"HTML, CSS",
+            src:"/images/jScript.jpg",
+            desc:"Embark on your journey into the world of web development by mastering the fundamental building blocks: HTML and CSS. In this beginner-friendly guide, you'll learn the essentials of structuring web pages with HTML tags and styling them with CSS properties."
+        },
+        {
+            title:"HTML/CSS",
+            src:"/images/react.jpg",
+            desc:"Embark on your journey into the world of web development by mastering the fundamental building blocks: HTML and CSS. In this beginner-friendly guide, you'll learn the essentials of structuring web pages with HTML tags and styling them with CSS properties."
+        },
+        {
+            title:"HTML, CSS",
+            src:"/images/jScript.jpg",
+            desc:"Embark on your journey into the world of web development by mastering the fundamental building blocks: HTML and CSS. In this beginner-friendly guide, you'll learn the essentials of structuring web pages with HTML tags and styling them with CSS properties."
+        },
+        {
+            title:"HTML, CSS",
+            src:"/images/htmlCss.jpg",
+            desc:"Embark on your journey into the world of web development by mastering the fundamental building blocks: HTML and CSS. In this beginner-friendly guide, you'll learn the essentials of structuring web pages with HTML tags and styling them with CSS properties."
+        },
+    ]
+  return (
+    <>
+      <div className="mt-24">
+        <div className="w-11/12 m-auto ps-16 pe-16 pt-5">
+            <h1 className="blogTitle">Boost up your skills with us</h1>
+            <div className="flex justify-between flex-wrap mb-5">
+            {cardData?.map((item)=>
+                <div className="border blogCard mt-7">
+                <div className="">
+                    <Image className="blogImage" alt={`${item?.title}`} src={`${item?.src}`} width={420} height={100}/>
+                </div>
+                <div className="flex p-4">
+                <h1 className="w-full pe-4 contentTitle">{item?.title}</h1>
+                </div>
+                <p className="p-2 ps-4 pe-4">
+                    {item?.desc}
+                </p>
+             </div>
+            )}
+            </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default blog;
--- src/components/ui/contact.tsx
+++ src/components/ui/contact.tsx
@@ -1,38 +1,106 @@
-import "../../../styles/contact.css"
+import "../../../styles/contact.css";
+import React, { useState } from "react";
 const contact = () => {
-    return (
-        <>
-         <div className="mt-32">
+  const [formData, setFormData] = useState({
+    studentName: "",
+    studentEmail: "",
+    studentPhone: "",
+    studentQuery: ""
+  });
+  const [formErrors, setFormErrors] = useState({});
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+    setFormErrors((prevErrors) => ({
+        ...prevErrors,
+        [name]: "",
+      }));
+};
+
+const validateForm = () =>{
+  const errors = {};
+  if (!formData?.studentName) {
+      errors.studentName = "Student name is required";
+  }if (!formData?.studentEmail) {
+      errors.studentEmail= " Student email is required";
+  }if (!formData?.studentPhone) {
+      errors.studentPhone = " Subject phone number  is required";
+    }
+
+  setFormErrors(errors);
+  return Object.keys(errors).length === 0;
+  } 
+ 
+const handleSubmit = async (e) => {
+  if(validateForm()){
+  
+}
+};
+  return (
+    <>
+      <div className="mt-32">
         <div className="w-11/12 m-auto ps-16 pe-16 pt-5 flex">
           <div>
             <div className="contactUsHeading">
-                <h1 className="">Why head into the battlefield unprepared?</h1>
-                <p className="w-4/5 mt-2">We work with ideas that inspire, engage and excite. Ideas that challenge convention and shape the trends. Through our awe-inspiring personal touch, we aim to make users fall in love with your brand.</p>
+              <h1 className="">Why head into the battlefield unprepared?</h1>
+              <p className="w-4/5 mt-2">
+                We work with ideas that inspire, engage and excite. Ideas that
+                challenge convention and shape the trends. Through our
+                awe-inspiring personal touch, we aim to make users fall in love
+                with your brand.
+              </p>
             </div>
             <div className="mt-14 flex">
               <div className="contactUs p-4">
                 <div>
-                    <input type="text" placeholder="Name" className="contactInput"></input>
+                  <input
+                    type="text"
+                    placeholder="Name"
+                    className="contactInput"
+                    name="studentName"
+                    value={formData?.studentName}
+                    onChange={(e) => handleChange(e)} 
+                  ></input>
+                  <span style={{ color: formErrors?.studentName ? 'red' : 'inherit' }}>{formErrors?.studentName}</span>
                 </div>
                 <div>
-                    <input type="text" placeholder="Email" className="contactInput"></input>
+                  <input
+                    type="text"
+                    placeholder="Email"
+                    className="contactInput"
+                    name="studentEmail"
+                    value={formData?.studentEmail}
+                    onChange={(e) => handleChange(e)} 
+                  ></input>
+                  <span style={{ color: formErrors?.studentEmail ? 'red' : 'inherit' }}>{formErrors?.studentEmail}</span>
                 </div>
                 <div>
-                <input type="number" placeholder="Phone" className="contactInput"></input>
+                  <input
+                    type="number"
+                    placeholder="Phone"
+                    className="contactInput"
+                    name="studentPhone"
+                    value={formData?.studentPhone}
+                    onChange={(e) => handleChange(e)} 
+                  ></input>
+                  <span style={{ color: formErrors?.studentPhone ? 'red' : 'inherit' }}>{formErrors?.studentPhone}</span>
                 </div>
               </div>
               <div className="yourQuery p-4">
-                  <textarea className="contactTextBox"></textarea>
+                <textarea className="contactTextBox" placeholder="Enter your Query" name="studentQuery"  onChange={(e) => handleChange(e)} ></textarea>
               </div>
             </div>
             <div className="flex justify-end pe-2">
-              <button type="button" className="btmSubmit"> SUBMIT APPLICATION</button>
+              <button type="button" className="btmSubmit" onClick={handleSubmit}>
+                {" "}
+                SUBMIT APPLICATION
+              </button>
             </div>
           </div>
         </div>
       </div>
-        </>
-    )
-}
+    </>
+  );
+};
 
 export default contact;
